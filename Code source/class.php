6a48eb9d2b6a9ec48733877bec43e72bec4b7294
yasmine <110662044+YAS15mine@users.noreply.github.com>
2023-04-28 05:01:40 -0700

    added by the one

--- Code source/class.php
+++ Code source/class.php
@@ -2,26 +2,49 @@
 
 class User
 {
-
   public $name;
   public $prenom;
   public $email;
   public $password;
 
-  public function signup($name, $prenom, $email, $password)
+  public function signup($name, $prenom, $email, $password, $conn)
   {
     $this->name = $name;
     $this->prenom = $prenom;
     $this->email = $email;
     $this->password = $password;
     $hashpass = $this->password($this->password);
-    $this->save($this->name,$this->prenom,$this->email,$hashpass);
+    $user = $this->check_user($this->email, $conn);
+    // echo "<pre>";
+    // var_dump($user);
+    // echo "<pre/>";
+    if ($user == false) {
+      $this->save($this->name, $this->prenom, $this->email, $hashpass, $conn);
+      $succes = "welcom to EduSphere comunity <br/> your account was created succefly ,sign in now !!";
+      return $succes;
+    } else {
+      $error = "This email is already used <br/> can you try to sign in to your account please !";
+      return $error;
+    }
   }
 
-  public function signin($email, $password)
+  public function signin($email, $password, $conn)
   {
     $this->email = $email;
     $this->password = $password;
+    $user = $this->check_user($this->email, $conn);
+    if ($user == false) {
+      $error = "This account is not exist <br> try to create an account if you  want to join the community ";
+      return $error;
+    } else {
+      $ex_pass = $user['password'];
+      $test = $this->test_pass($this->password, $ex_pass);
+      if ($test) {
+        session_start();
+        $_SESSION['id'] = $user['Id_apprenant_'];
+        header("Location: apphome.html");
+      }
+    }
   }
 
 
@@ -31,11 +54,23 @@ class User
     return $hashvalue;
   }
 
-
-  public function save($name, $prenom, $email, $password)
+  public function test_pass($password, $ex_pass)
   {
-    $sql = "INSERT INTO `apprenant_` ('name','prenom','email', 'password') VALUES ('$name','$prenom','$email','$password')";
-    echo $sql;
-    // $stmt = $conn->query($sql);
+      $result = password_verify($password, $ex_pass);
+      return $result;
+  }
+  
+  public function check_user($email, $conn)
+  {
+    $sql = "SELECT * FROM `apprenant_` WHERE `email` = '$email'";
+    $user = $conn->query($sql);
+    $user = $user->fetch(PDO::FETCH_ASSOC);
+    return $user;
+  }
+
+  public function save($name, $prenom, $email, $password, $conn)
+  {
+    $sql = "INSERT INTO `apprenant_` (`nom_`,`prenom`,`email`, `password`) VALUES ('$name','$prenom','$email','$password')";
+    $conn->query($sql);
   }
 }
--- Code source/index.php
+++ Code source/index.php
@@ -9,22 +9,20 @@ function test_input($data)
   return $data;
 }
 if (isset($_POST['sign_up'])) {
-  echo "it's working";
-
-
-
   $name = test_input($_POST['name']);
   $prenom = test_input($_POST['prenom']);
   $mail = test_input($_POST['emaile']);
   $password = test_input($_POST['passwrd']);
+  $signup = new User();
+  $result = $signup->signup($name, $prenom, $mail, $password, $conn);
+  // echo $signup->signup($name, $prenom, $mail, $password);
+}
 
-
-  $signup = new User(); 
-  $signup->signup($name, $prenom, $mail, $password);
-  echo $signup->signup($name, $prenom, $mail, $password);
-
-
-
+if (isset($_POST['sign_in'])) {
+  $email = test_input(($_POST['email']));
+  $password = test_input(($_POST['password']));
+  $signin = new User();
+  $result = $signin->signin($email, $password, $conn);
 }
 
 ?>
@@ -39,58 +37,62 @@ if (isset($_POST['sign_up'])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="apprenant.CSS">
-  <title>Document</title>
+  <title>Sign up</title>
 </head>
 
 <body>
-
-
-
-
-
-  <div class="form">
-    <form action="" method="post">
-      <input id="noaccount" name="radioaccount" type="radio" class="radio radio--invisible" checked />
-      <input id="account" name="radioaccount" type="radio" class="radio radio--invisible" />
-      <div class="form_background">
-        <div class="form-group form-group--account">
-          <h3 class="form-group_title">sign in</h3>
-          <input class="form-group_input" type="email" name="email" placeholder="Email" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-          <input class="form-group_input" type="password" name="password" placeholder="Password" required="" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,16}$">
-          <button class="button button--form" type="submit">Sign in</button>
+  <?php
+  if (isset($result)) {
+  ?>
+    <div class="alert alert-warning text-center text-dark" role="alert">
+    <?php
+    echo $result;
+  }
+    ?>
+    </div>
+    <div class="form">
+      <form action="" method="post">
+        <input id="noaccount" name="radioaccount" type="radio" class="radio radio--invisible" checked />
+        <input id="account" name="radioaccount" type="radio" class="radio radio--invisible" />
+        <div class="form_background">
+          <div class="form-group form-group--account">
+            <h3 class="form-group_title">sign in</h3>
+            <input class="form-group_input" type="email" name="email" placeholder="Email" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+            <input class="form-group_input" type="password" name="password" placeholder="Password" required="" >
+            <button name="sign_in" class="button button--form" type="submit">Sign in</button>
+          </div>
+      </form>
+      <form action="" method="post">
+        <div class="form-group form-group--noaccount">
+          <h3 class="form-group_title">Sign up</h3>
+          <input class="form-group_input" type="text" name="name" placeholder="First Name" required="">
+          <input class="form-group_input" type="text" name="prenom" placeholder="last Name" required="">
+          <input class="form-group_input" type="email" name="emaile" placeholder="Email" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
+          <!-- * Pa$$w0rd!  T1s1s@s3cur3 -->
+          <input class="form-group_input" type="password" name="passwrd" placeholder="Password" required="">
+          <button class="button button--form" type="submit" name="sign_up">Sign up</button>
         </div>
-    </form>
-    <form action="" method="post">
-      <div class="form-group form-group--noaccount">
-        <h3 class="form-group_title">Sign up</h3>
-        <input class="form-group_input" type="text" name="name" placeholder="First Name" required="">
-        <input class="form-group_input" type="text" name="prenom" placeholder="last Name" required="">
-        <input class="form-group_input" type="email" name="emaile" placeholder="Email" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$">
-        <!-- * Pa$$w0rd!  T1s1s@s3cur3 -->
-        <input class="form-group_input" type="password" name="passwrd" placeholder="Password" required="">
-        <button class="button button--form" type="submit" name="sign_up">Sign up</button>
+      </form>
+    </div>
+
+    <fieldset class="fieldset">
+      <h2>Don't have an account ?</h2>
+      <p>Are you looking for a way to improve your education and training? If so, then you need to check out our center of formation!.So what are you waiting for?
+        Sign up today and start your journey to success!</p>
+      <div class="form_content form_content--noaccount">
+
       </div>
-    </form>
-  </div>
+      <label for="noaccount" class="button">Signup</label>
+    </fieldset>
+    <fieldset class="fieldset">
+      <h2>Have an account ?</h2>
+      <p>Please sign in to access your account.</p>
+      <div class="form_content form_content--noaccount">
 
-  <fieldset class="fieldset">
-    <h2>Don't have an account ?</h2>
-    <p>Are you looking for a way to improve your education and training? If so, then you need to check out our center of formation!.So what are you waiting for?
-      Sign up today and start your journey to success!</p>
-    <div class="form_content form_content--noaccount">
-
+      </div>
+      <label for="account" class="button">sign in</label>
+    </fieldset>
     </div>
-    <label for="noaccount" class="button">Signup</label>
-  </fieldset>
-  <fieldset class="fieldset">
-    <h2>Have an account ?</h2>
-    <p>Please sign in to access your account.</p>
-    <div class="form_content form_content--noaccount">
-
-    </div>
-    <label for="account" class="button">sign in</label>
-  </fieldset>
-  </div>
 
 
 </body>
